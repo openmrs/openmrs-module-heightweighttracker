e963e7745431079b5cb8981f8da047650b0ec12e
rubailly <rubailly@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-10-30 08:11:51 +0000

    mavenized the module and increased module version

---
+++ api/src/main/java/org/openmrs/module/heightweighttracker/impl/pih/ConceptDictionary.java
@@ -0,0 +1,41 @@
+package org.openmrs.module.heightweighttracker.impl.pih;
+
+import org.openmrs.module.heightweighttracker.MetadataHelper;
+
+
+/**
+ * Class mirrors the concept ids in the PIH Concept dictionary. NOTE: We would
+ * want an abstraction layer, however, this is being done specifically for
+ * Rwanda PIH.
+ */
+public class ConceptDictionary {
+	
+	
+
+	public static int HEIGHT_CM ;
+	
+	public static int WEIGHT_KG;
+	
+	public static int Z_SCORE_WEIGHT;
+	
+	public static int Z_SCORE_HEIGHT;
+	
+	public static int HEIGHT_WEIGHT_PERCENTILE;
+	
+	public static int NEW_FORM;
+	
+	public static int GROWTH_ENCOUNTER;
+	
+	public static int BMI;
+	
+	public static int Z_SCORE_BMI;
+	
+	public static int PatientIdentiferId_IMB;
+
+	public static int PatientIdentiferId_PC;
+	
+	private ConceptDictionary() {
+	    MetadataHelper.setupConstants();
+	}
+
+}
